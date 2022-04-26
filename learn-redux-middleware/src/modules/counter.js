648a56649faa2423542741e1c43e83647a5d483a
S-YC <s9292909@gmail.com>
2022-04-26 21:51:37 +0900

    비동기 작업에 대한 미들웨어 사용

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,12 +1,25 @@
-import { createAction, handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions"
 
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
+const INCREASE = 'counter/INCREASE'
+const DECREASE = 'counter/DECREASE'
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-const initalState = 0;
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
+export const increaseAsync = () => dispatch => {
+        setTimeout(() => {
+            dispatch(increase())
+        }, 1000)
+}
+
+export const decreaseAsync = () => dispatch => {
+    setTimeout(() => {
+        dispatch(decrease())
+    }, 1000)
+};
+
+const initalState = 0; //객체값이 아닌 숫자도 잘 작동합니다.
 
 const counter = handleActions(
     {
