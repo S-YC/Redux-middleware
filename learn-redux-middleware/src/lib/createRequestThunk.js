c2a67b526db599de3189e6bf0b668abe677c2d38
S-YC <s9292909@gmail.com>
2022-05-02 12:19:49 +0900

    redux-thunk 리펙토링 작업

--- learn-redux-middleware/src/lib/createRequestThunk.js
+++ learn-redux-middleware/src/lib/createRequestThunk.js
@@ -1,8 +1,9 @@
+import { startLoading, finishLoading } from "../modules/loading"
+
 export default function createRequestThunk(type, request){
     // 성공 및 실패 액션타입 정의
     const SUCCESS = `${type}_SUCCESS`
     const FAILURE = `${type}_FAILURE`
-    console.log(SUCCESS)
     return parms => async dispatch => {
         dispatch({ type }) //시작
         try {
@@ -11,12 +12,14 @@ export default function createRequestThunk(type, request){
                 type: SUCCESS,
                 payload: response.data
             }) // 성공
+            dispatch(finishLoading(type))
         } catch (e) {
             dispatch({
                 type: FAILURE,
                 payload: e,
                 error: true 
             }) // 에러
+            dispatch(startLoading(true))
             throw e 
         }
     }
